8be8733fcfc838fb4f24f5514d6583393cc37e30
devhomie <173510882+devhomie@users.noreply.github.com>
2024-08-09 11:56:46 -0400

    line-chart.js - setting up margins for line chart

--- line-chart.js
+++ line-chart.js
@@ -1,6 +1,19 @@
 // Load the data here
+d3.csv("./data/weekly_temperature.csv", d3.autoType).then((data) => {
+  console.log("temperature data", data);
+  drawLineChart(data);
+});
+
+let hi = "What IS UPPP!";
+
+console.log(hi);
+console.log(d3.csv("./data/weekly_temperature.csv", d3.autoType));
 
 // Create the line chart here
 const drawLineChart = (data) => {
-
+  const margin = { top: 40, right: 170, bottom: 25, left: 40 };
+  const width = 1000;
+  const height = 500
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
 };
