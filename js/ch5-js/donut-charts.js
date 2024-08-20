48986f43a02bf8779f358cc724a9f877bd8cf0c4
devhomie <173510882+devhomie@users.noreply.github.com>
2024-08-20 13:05:01 -0400

    Formatted data from CSV file in donut-chart.js
    After pushing the formatted data in to array `formattedData` , I've console.log to observe the data
    
    initialized the `xScale` constant as `.scaleBand()` object in scales.js file, chain the `.domain` and returning the result of the `.range`

--- js/ch5-js/donut-charts.js
+++ js/ch5-js/donut-charts.js
@@ -1,5 +1,35 @@
 const drawDonutCharts = (data) => {
   // Generate the donut charts here
+  const svg = d3
+    .select("#donut")
+    .append("svg")
+    .attr("viewBox", ` 0 0 ${width} ${height}`);
 
-  
+  const donutContainers = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  const years = [1975, 1995, 2013];
+
+  // Extract the formats from the columns attribute of the data, and filter out the "year" item
+  const formats = data.columns.filter((format) => format !== "year");
+
+  years.forEach((year) => {
+    const donutContainer = donutContainers
+      .append("g")
+      .attr("transform", `translate(${xScale(year)}, ${innerHeight / 2})`);
+
+    // Retrieve the data related to the year of interest.
+    const yearData = data.find((d) => d.year === year);
+
+    // Initialize an empty array for the formatted data
+    const formattedData = [];
+
+    // for each format, create an object containing both the format ID and its sales for the year of interest. 
+    // `.push` Pushing these objects to the empty formatted data array initialized above
+    formats.forEach((format) => {
+      formattedData.push({ format: format, sales: yearData[format] });
+    });
+    // console.log(formattedData);
+    
+  });
 };
